7d3d1d13b6667547c5fdb21c4f1e2d3bb131ed71
MatusGuy <mateu.axe@gmail.com>
2022-06-08 23:02:56 +0100

    wip get inherited members

--- MtObjectBrowser.lua
+++ MtObjectBrowser.lua
@@ -94,143 +94,6 @@ end
 
 type ObjectList = {[string]: Object}
 
-local STROKE = Instance.new("UIStroke")
-STROKE.Name = "Stroke"
-STROKE.Thickness = 3
-STROKE.LineJoinMode = Enum.LineJoinMode.Miter
-STROKE.Color = Color3.fromRGB(130, 130, 130)
-
-local function AddStroke(frame:Frame, useUIStroke:boolean)
-	useUIStroke = useUIStroke or false
-
-	if useUIStroke then
-		STROKE:Clone().Parent = frame
-	else
-		frame.BorderSizePixel = STROKE.Thickness
-		frame.BorderColor3 = STROKE.Color
-		frame.BorderMode = Enum.BorderMode.Inset
-	end
-end
-
-local function LoadClassInfoGui(scrollarea:ScrollingFrame, desclabel:TextLabel, order:number, name:string, info:Object|Member, use_fontsize:boolean): TextButton
-	use_fontsize = use_fontsize or true
-
-	local button = Instance.new("TextButton")
-	button.Name = name
-	button.Size = UDim2.new(1,0,0,25)
-	button.BackgroundColor3 = scrollarea.BackgroundColor3
-	button.BorderSizePixel = 0
-	button.Font = Enum.Font.SourceSans
-	if use_fontsize then
-		button.TextSize = 15
-	else
-		button.TextScaled = true
-	end
-
-	button.TextColor3 = Color3.new(1, 1, 1)
-	button.TextXAlignment = Enum.TextXAlignment.Left
-	button.Text = "         "..name -- TODO: find a way to not do this
-	button.LayoutOrder = order
-	button.Parent = scrollarea
-
-
-	local icon = Instance.new("ImageLabel")
-	icon.Name = "Icon"
-	icon.Size = UDim2.new(0,25,1,0)
-	icon.BackgroundTransparency = 1
-
-	icon.ScaleType = Enum.ScaleType.Fit
-	icon.Image = info.Icon
-	icon.Parent = button
-
-	button.MouseButton1Down:Connect(function()
-		for _, button in ipairs(scrollarea:GetChildren()) do
-			if button:IsA("GuiButton") then
-				button.BackgroundColor3 = Color3.fromRGB(46,46,46)
-			end
-		end
-		button.BackgroundColor3 = Color3.fromRGB(0, 92, 179)
-
-		--desclabel.Text = '<b><font size="30">'..name.."</font></b><br/><br/>"..(info.Description or "<i>This class/member seems to be undocumented.</i>")
-		desclabel.Text = GetFullDesc(name,info)
-	end)
-
-	return button
-end
-
-local function CreateScrollArea(name: string, pos:UDim2, size:UDim2, screen): ScrollingFrame
-	screen = screen or mainscreen
-
-	local area = Instance.new("ScrollingFrame")
-	area.Name = name
-	area.Position = pos
-	area.Size = size
-	area.BackgroundColor3 = Color3.fromRGB(46,46,46)
-	area.ElasticBehavior = Enum.ElasticBehavior.WhenScrollable
-	AddStroke(area)
-	area.Parent = screen
-
-	local layout = Instance.new("UIListLayout")
-	layout.Name = "ListLayout"
-	layout.FillDirection = Enum.FillDirection.Vertical
-	layout.SortOrder = Enum.SortOrder.LayoutOrder
-	layout.Padding = UDim.new(0,0)
-	layout.Parent = area
-
-	return area
-end
-
-local function InitGui(objects:ObjectList, screen:ScreenGui)
-	screen = screen or mainscreen
-
-	local objectsarea = CreateScrollArea("ObjectsArea",UDim2.fromScale(0,0),UDim2.fromScale(0.225,1), screen)
-	local membersarea = CreateScrollArea("MembersArea",UDim2.fromScale(0.225,0),UDim2.fromScale(0.775,0.6), screen)
-
-	local descarea    = CreateScrollArea("DescArea",UDim2.fromScale(0.225,0.6),UDim2.fromScale(0.775,0.4), screen)
-
-	local descpadding = Instance.new("UIPadding")
-	descpadding.Name = "Padding"
-	local padding = UDim.new(0,5)
-	descpadding.PaddingTop = padding
-	descpadding.PaddingBottom = padding
-	descpadding.PaddingLeft = padding
-	descpadding.PaddingRight = padding
-	descpadding.Parent = descarea
-
-	local desclabel = Instance.new("TextLabel")
-	desclabel.Name = "DescLabel"
-	desclabel.Size = UDim2.fromScale(1,1)
-	desclabel.BackgroundTransparency = 1
-	desclabel.TextColor3 = Color3.new(1,1,1)
-	desclabel.TextXAlignment = Enum.TextXAlignment.Left
-	desclabel.TextYAlignment = Enum.TextYAlignment.Top
-	desclabel.TextSize = 15
-	desclabel.TextWrapped = true
-	desclabel.Font = Enum.Font.SourceSans
-	desclabel.RichText = true
-	desclabel.Text = "<i>Press an object in the panel on the left to look at its description!</i>"
-	desclabel.Parent = descarea
-
-	local order = 0
-	for name, info in pairs(objects) do
-		order += 1
-
-		local button = LoadClassInfoGui(objectsarea,desclabel,order,name,info)
-			button.MouseButton1Down:Connect(function()
-				-- destroy all buttons inside membersarea
-				for _,v in ipairs(membersarea:GetChildren()) do if v:IsA("GuiButton") then v:Destroy() end end
-
-				if info.Members then
-					local morder = 0
-					for name, info in pairs(info.Members) do
-						morder += 1
-						LoadClassInfoGui(membersarea,desclabel,morder,name,info)
-					end
-				end
-			end)
-	end
-end
-
 local OBJECTS = {
 	MObject = {
 		Icon = OBJECT_ICON,
@@ -301,6 +164,158 @@ local OBJECTS = {
 		Description = nil
 	},
 }
+
+local STROKE = Instance.new("UIStroke")
+STROKE.Name = "Stroke"
+STROKE.Thickness = 3
+STROKE.LineJoinMode = Enum.LineJoinMode.Miter
+STROKE.Color = Color3.fromRGB(130, 130, 130)
+
+local function AddStroke(frame:Frame, useUIStroke:boolean)
+	useUIStroke = useUIStroke or false
+
+	if useUIStroke then
+		STROKE:Clone().Parent = frame
+	else
+		frame.BorderSizePixel = STROKE.Thickness
+		frame.BorderColor3 = STROKE.Color
+		frame.BorderMode = Enum.BorderMode.Inset
+	end
+end
+
+local function LoadClassInfoGui(scrollarea:ScrollingFrame, desclabel:TextLabel, order:number, name:string, info:Object|Member, use_fontsize:boolean): TextButton
+	use_fontsize = use_fontsize or true
+
+	local button = Instance.new("TextButton")
+	button.Name = name
+	button.Size = UDim2.new(1,0,0,25)
+	button.BackgroundColor3 = scrollarea.BackgroundColor3
+	button.BorderSizePixel = 0
+	button.Font = Enum.Font.SourceSans
+	if use_fontsize then
+		button.TextSize = 15
+	else
+		button.TextScaled = true
+	end
+
+	button.TextColor3 = Color3.new(1, 1, 1)
+	button.TextXAlignment = Enum.TextXAlignment.Left
+	button.Text = "         "..name -- TODO: find a way to not do this
+	button.LayoutOrder = order
+	button.Parent = scrollarea
+
+
+	local icon = Instance.new("ImageLabel")
+	icon.Name = "Icon"
+	icon.Size = UDim2.new(0,25,1,0)
+	icon.BackgroundTransparency = 1
+
+	icon.ScaleType = Enum.ScaleType.Fit
+	icon.Image = info.Icon
+	icon.Parent = button
+
+	button.MouseButton1Down:Connect(function()
+		for _, button in ipairs(scrollarea:GetChildren()) do
+			if button:IsA("GuiButton") then
+				button.BackgroundColor3 = Color3.fromRGB(46,46,46)
+			end
+		end
+		button.BackgroundColor3 = Color3.fromRGB(0, 92, 179)
+
+		desclabel.Text = GetFullDesc(name,info)
+	end)
+
+	return button
+end
+
+local function CreateScrollArea(name: string, pos:UDim2, size:UDim2, screen): ScrollingFrame
+	screen = screen or mainscreen
+
+	local area = Instance.new("ScrollingFrame")
+	area.Name = name
+	area.Position = pos
+	area.Size = size
+	area.BackgroundColor3 = Color3.fromRGB(46,46,46)
+	area.ElasticBehavior = Enum.ElasticBehavior.WhenScrollable
+	AddStroke(area)
+	area.Parent = screen
+
+	local layout = Instance.new("UIListLayout")
+	layout.Name = "ListLayout"
+	layout.FillDirection = Enum.FillDirection.Vertical
+	layout.SortOrder = Enum.SortOrder.LayoutOrder
+	layout.Padding = UDim.new(0,0)
+	layout.Parent = area
+
+	return area
+end
+
+local function TableMerge(a:{},b:{}):{}
+	for i,v in ipairs(b) do a[i] = v end
+end
+
+local function GetInheritedMembers(parent:string): {Member}
+	local parentMembers = {}
+
+	if OBJECTS[parent].Parent then
+		parentMembers = GetInheritedMembers(OBJECTS[parent].Parent)
+	end
+
+	return TableMerge(OBJECTS[parent].Members,parentMembers)
+end
+
+local function InitGui(objects:ObjectList, screen:ScreenGui)
+	screen = screen or mainscreen
+
+	local objectsarea = CreateScrollArea("ObjectsArea",UDim2.fromScale(0,0),UDim2.fromScale(0.225,1), screen)
+	local membersarea = CreateScrollArea("MembersArea",UDim2.fromScale(0.225,0),UDim2.fromScale(0.775,0.6), screen)
+
+	local descarea    = CreateScrollArea("DescArea",UDim2.fromScale(0.225,0.6),UDim2.fromScale(0.775,0.4), screen)
+
+	local descpadding = Instance.new("UIPadding")
+	descpadding.Name = "Padding"
+	local padding = UDim.new(0,5)
+	descpadding.PaddingTop = padding
+	descpadding.PaddingBottom = padding
+	descpadding.PaddingLeft = padding
+	descpadding.PaddingRight = padding
+	descpadding.Parent = descarea
+
+	local desclabel = Instance.new("TextLabel")
+	desclabel.Name = "DescLabel"
+	desclabel.Size = UDim2.fromScale(1,1)
+	desclabel.BackgroundTransparency = 1
+	desclabel.TextColor3 = Color3.new(1,1,1)
+	desclabel.TextXAlignment = Enum.TextXAlignment.Left
+	desclabel.TextYAlignment = Enum.TextYAlignment.Top
+	desclabel.TextSize = 15
+	desclabel.TextWrapped = true
+	desclabel.Font = Enum.Font.SourceSans
+	desclabel.RichText = true
+	desclabel.Text = "<i>Press an object in the panel on the left to look at its description!</i>"
+	desclabel.Parent = descarea
+
+	local order = 0
+	for name, info in pairs(objects) do
+		order += 1
+
+		local button = LoadClassInfoGui(objectsarea,desclabel,order,name,info)
+		button.MouseButton1Down:Connect(function()
+			-- destroy all buttons inside membersarea
+			for _,v in ipairs(membersarea:GetChildren()) do if v:IsA("GuiButton") then v:Destroy() end end
+			
+			local inherited = GetInheritedMembers(info.Parent)
+
+			if info.Members then
+				local morder = 0
+				for name, info in pairs(TableMerge(info.Members,inherited)) do
+					morder += 1
+					LoadClassInfoGui(membersarea,desclabel,morder,name,info)
+				end
+			end
+		end)
+	end
+end
 
 InitGui(OBJECTS)
 
